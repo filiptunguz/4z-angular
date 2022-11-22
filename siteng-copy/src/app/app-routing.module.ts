36214d489684dae302b98fbc80e746280a4e9e61
Filip <filipkralh11@gmail.com>
2022-11-22 11:40:29 +0100

    home component & home route

--- siteng-copy/src/app/app-routing.module.ts
+++ siteng-copy/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./home/home.component').then(c => c.HomeComponent),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
