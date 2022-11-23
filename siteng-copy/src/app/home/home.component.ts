41339421cc8f54072ce5b2ad734376cd9675c6f1
Filip <filipkralh11@gmail.com>
2022-11-23 10:31:30 +0100

    BORING: remove load user from home component

--- siteng-copy/src/app/home/home.component.ts
+++ siteng-copy/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {HeaderComponent} from "../app-layout/header/header.component";
-import {AuthService} from "../auth/services/auth.service";
 
 @Component({
   selector: 'app-home',
@@ -10,10 +9,4 @@ import {AuthService} from "../auth/services/auth.service";
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
-  authService = inject(AuthService);
-
-  loadUser() {
-    this.authService.loadUserFromApiKey();
-  }
-}
+export class HomeComponent {}
